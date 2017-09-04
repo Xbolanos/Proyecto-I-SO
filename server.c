d1db164b05c48689aa330f5ca2563fa72325cca7
Ariana B <bermudezarii@gmail.com>
2017-09-04 02:01:23 -0700

    Continuar aceptando msg

--- server.c
+++ server.c
@@ -45,28 +45,30 @@ int main(int argc, char *argv[])
 	listen(sockfd, 5);
 	clilen= sizeof(cli_addr);
 
-	newsockfd= accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	while(1){
+		newsockfd= accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-	if( newsockfd < 0)
-	{
-		error("ERROR on accept");
-	}
+		if( newsockfd < 0)
+		{
+			error("ERROR on accept");
+		}
 
-	bzero(buffer, 256);
-	n= read(newsockfd, buffer, 255);
+		bzero(buffer, 256);
+		n= read(newsockfd, buffer, 255);
 
-	if( n < 0 )
-	{
-		error("ERROR reading from socket");
-	}
+		if( n < 0 )
+		{
+			error("ERROR reading from socket");
+		}
 
-	printf("Here is the message: %s\n", buffer);
+		printf("Here is the message: %s\n", buffer);
 
-	n= write(newsockfd, "I got your message",18);
+		n= write(newsockfd, "I got your message",18);
 
-	if (n < 0)
-	{
-		error("ERROR writing to socket");
+		if (n < 0)
+		{
+			error("ERROR writing to socket");
+		}
 	}
 
 	return 0;
